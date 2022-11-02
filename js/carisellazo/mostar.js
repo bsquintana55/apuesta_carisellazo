14b9158c4749ad10dabcb609961273ad886472d2
bsquintana55 <bsquintana55@misena.edu.co>
2022-11-01 22:50:38 -0500

    estilos js de las vistas

--- js/carisellazo/mostar.js
+++ js/carisellazo/mostar.js
@@ -6,3 +6,17 @@ let moneda = document.getElementById("moneda")
 let mano = document.getElementById("mano")
 let todo = document.getElementById("todo")
 
+
+jugar.style.display = "none"
+todo.style.display = "none"
+moneda.style.display = "none"
+
+iniciar.addEventListener("click", () => {
+    mano.style.display = "none"
+    moneda.style.display = "block"
+    todo.style.display = "block"
+    iniciar.style.display = "none"
+    jugar.style.display = "block"
+    seguir.style.display = "none"
+})
+
