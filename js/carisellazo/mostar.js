724e7fbe4a6c8f277474e527cfa81a3f392ee7a2
bsquintana55 <bsquintana55@misena.edu.co>
2022-11-01 22:50:07 -0500

    declarar variables

---
+++ js/carisellazo/mostar.js
@@ -0,0 +1,8 @@
+let jugar = document.getElementById("jugar")
+let iniciar = document.getElementById("iniciar")
+let seguir = document.getElementById("segunda")
+
+let moneda = document.getElementById("moneda")
+let mano = document.getElementById("mano")
+let todo = document.getElementById("todo")
+
